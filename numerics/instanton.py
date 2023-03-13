e0ff208965b4c962982f5d18a5b3c4c8655f2901
vetkje <larsgallardo@hotmail.no>
2023-03-13 14:38:49 +0100

    ?

--- numerics/instanton.py
+++ numerics/instanton.py
@@ -19,20 +19,47 @@ We use the system vector s of the form:
 class NG_memory:
     def __init__(
         self,
-        a=10,
-        b=2,
-        sigma=1,
-        lam=1,
-        D1=1,
-        D2=1,
-        kappa=1,
+        a=10.0,
+        b=2.0,
+        sigma=1.0,
+        lam=1.0,
+        D1=1.0,
+        D2=1.0,
+        kappa=1.0,
         noise="g",
         pot="m",
         dx=1e-4,
         boundary_cond=[-1, 0, 0, 0],
         number_timestep=30,
-        maxtime=1,
-    ) -> None:
+        maxtime=1.0,
+    ):
+        """Initialisation of the class
+
+        Args:
+            a (float, optional): rescaled variance of characteristic funtion. Defaults to 10.0.
+            b (float, optional): parameter for characteristic function: for details see individual functions. Defaults to 2.0.
+            sigma (float, optional): variance for gaussian characteristic function. Defaults to 1.0.
+            lam (float, optional): poisson shot noise rate. Defaults to 1.0.
+            D1 (float, optional): gaussian noise variance on q. Defaults to 1.0.
+            D2 (float, optional): gaussian noise variance on y. Defaults to 1.0.
+            kappa (float, optional): inverse OU memory timescale. Defaults to 1.0.
+            noise (str, optional): type amplitude distribution, choice between:
+                    g for Gaussian,
+                    d for delta,
+                    e for two-sided exponential,
+                    g for gamma,
+                    t for truncated.
+                    Defaults to "g".
+            pot (str, optional): type of potential, choice between:
+                    m for mexican hat.
+                    Defaults to "m".
+            dx (float, optional): discretisation for derivatives. Defaults to 1e-4.
+            boundary_cond (list, optional): boundary conditions of form
+                    [q_initial, q_final, y_initial, y_final].
+                    Defaults to [-1, 0, 0, 0].
+            number_timestep (int, optional): number of timesteps for instanton. Defaults to 30.0.
+            maxtime (float, optional): maximal time. Defaults to 1.0.
+        """
         self.a = a
         self.lamb = lam
         self.D1 = D1
@@ -66,25 +93,83 @@ class NG_memory:
         self.number_timestep = number_timestep
         self.maxtime = maxtime
 
-    def Pot_mexico(self, x) -> float:
+    def Pot_mexico(self, x):
+        """*mexican* hat potential
+
+        Args:
+            x (np.ndarray): position
+
+        Returns:
+            np.ndarray: value of potential at positions
+        """
         return x**4 / 4 - x**2 / 2
 
-    def Phi_gauss(self, x) -> float:
-        return np.exp(x**2 * self.sigma**2 / 2)
+    def Phi_gauss(self, x):
+        """characteristic function for gaussian distribution
 
-    def Phi_delta(self, x) -> float:
+        Args:
+            x (float): position at which to evaluate the characteric function
+
+        Returns:
+            float: value of characteristic function
+        """
+        return np.exp(x**2 * self.sigma**2 / 2.0)
+
+    def Phi_delta(self, x):
+        """characteristic function for symmetric delta distributed
+
+        Args:
+            x (float): position at which to evaluate the characteric function
+
+        Returns:
+            float: value of characteristic function
+        """
         return np.cosh(x) - 1
 
-    def Phi_exp(self, x) -> float:
+    def Phi_exp(self, x):
+        """characteristic function for symmetric exponential distribution
+
+        Args:
+            x (float): position at which to evaluate the characteric function
+
+        Returns:
+            float: value of characteristic function
+        """
         return x**2 / (2 * (1 - x**2))
 
-    def Phi_gamma(self, x) -> float:
+    def Phi_gamma(self, x):
+        """characteristic function for gamma distribution
+
+        Args:
+            x (float): position at which to evaluate the characteric function
+
+        Returns:
+            float: value of characteristic function
+        """
         return ((1 + x) ** self.b + (1 - x) ** self.b - 2) / (2 * self.b * (self.b - 1))
 
-    def Phi_truncated(self, x) -> float:
+    def Phi_truncated(self, x):
+        """characteristic function for truncated distribution
+
+        Args:
+            x (float): position at which to evaluate the characteric function
+
+        Returns:
+            float: value of characteristic function
+        """
         return 0.5 * x**2 + self.b * x**4
 
-    def F(self, t, s, p) -> np.ndarray:
+    def F(self, t, s, p):
+        """System function
+
+        Args:
+            t (np.ndarray): time
+            s (np.ndarray): system array
+            p (list): initial parameters to be determined
+
+        Returns:
+            np.ndarray: system equations
+        """
         # q coordinate
         output1 = (
             +s[2] * self.D1 + s[1] - misc.derivative(self.potential, s[0], dx=self.dx)
@@ -106,7 +191,17 @@ class NG_memory:
         # output
         return np.vstack((output1, output2, output3, output4))
 
-    def Residuals(self, ini, fin, p) -> np.ndarray:
+    def Residuals(self, ini, fin, p):
+        """Residuals for boundary conditions
+
+        Args:
+            ini (np.ndarray): initial values
+            fin (np.ndarray): final values
+            p (list): initial parameters to be determined
+
+        Returns:
+            np.ndarray: residuals
+        """
         k = p[0]
         k2 = p[1]
         res = np.zeros(6)
@@ -120,6 +215,12 @@ class NG_memory:
         return res
 
     def instanton(self):
+        """Function to recover the instanton of the system given some initial conditions. The method used is the
+        boundary value problem solver by scipy, see shooting method
+
+        Returns:
+            scipy.integrate._bvp.BVPResult: Result from the integrator
+        """
         # time
         t = np.linspace(0, self.maxtime, self.number_timestep)
 
@@ -129,14 +230,229 @@ class NG_memory:
         y[0, 0] = self.boundary_cond[0]
         y[1, 0] = self.boundary_cond[2]
 
-        result = solve_bvp(self.F, self.Residuals, t, y, p=[0.2, 0.2], verbose=0)
+        result = solve_bvp(self.F, self.Residuals, t, y, p=[0.2, 0.2], verbose=2)
 
         print(result.message)
-        return result
+        return result, t
 
 
-test = NG_memory()
+class NG_no_memory:
+    def __init__(
+        self,
+        a=10.0,
+        b=2.0,
+        sigma=1.0,
+        lam=1.0,
+        D1=1.0,
+        noise="g",
+        pot="m",
+        dx=1e-4,
+        boundary_cond=[-1, 0, 0.2],
+        number_timestep=30,
+        maxtime=1.0,
+    ):
+        """Initialisation of the class
 
-work = test.instanton()
+        Args:
+            a (float, optional): rescaled variance of characteristic funtion. Defaults to 10.0.
+            b (float, optional): parameter for characteristic function: for details see individual functions. Defaults to 2.0.
+            sigma (float, optional): variance for gaussian characteristic function. Defaults to 1.0.
+            lam (float, optional): poisson shot noise rate. Defaults to 1.0.
+            D1 (float, optional): gaussian noise variance on q. Defaults to 1.0.
+            noise (str, optional): type amplitude distribution, choice between:
+                    g for Gaussian,
+                    d for delta,
+                    e for two-sided exponential,
+                    g for gamma,
+                    t for truncated.
+                    Defaults to "g".
+            pot (str, optional): type of potential, choice between:
+                    m for mexican hat.
+                    Defaults to "m".
+            dx (float, optional): discretisation for derivatives. Defaults to 1e-4.
+            boundary_cond (list, optional): boundary conditions of form
+                    [q_initial, q_final, y_initial, y_final].
+                    Defaults to [-1, 0, 0, 0].
+            number_timestep (int, optional): number of timesteps for instanton. Defaults to 30.0.
+            maxtime (float, optional): maximal time. Defaults to 1.0.
+        """
+        self.a = a
+        self.lamb = lam
+        self.D1 = D1
+        self.dx = dx
 
-print(work.message)
+        # noise parameters (if needed)
+        self.b = b
+        self.sigma = sigma
+
+        # characteristic function of noise
+        if noise == "g":
+            self.phi = self.Phi_gauss
+        elif noise == "d":
+            self.phi = self.Phi_delta
+        elif noise == "e":
+            self.phi = self.Phi_exp
+        elif noise == "g":
+            self.phi = self.Phi_gamma
+        elif noise == "t":
+            self.phi = self.Phi_truncated
+
+        # potential
+        if pot == "m":
+            self.potential = self.Pot_mexico
+        # TODO add more potentials (skewed, or other?)
+
+        # system parameters
+        self.boundary_cond = boundary_cond
+        self.number_timestep = number_timestep
+        self.maxtime = maxtime
+
+    def Pot_mexico(self, x):
+        """*mexican* hat potential
+
+        Args:
+            x (np.ndarray): position
+
+        Returns:
+            np.ndarray: value of potential at positions
+        """
+        return x**4 / 4 - x**2 / 2
+
+    def Phi_gauss(self, x):
+        """characteristic function for gaussian distribution
+
+        Args:
+            x (float): position at which to evaluate the characteric function
+
+        Returns:
+            float: value of characteristic function
+        """
+        return np.exp(x**2 * self.sigma**2 / 2.0)
+
+    def Phi_delta(self, x):
+        """characteristic function for symmetric delta distributed
+
+        Args:
+            x (float): position at which to evaluate the characteric function
+
+        Returns:
+            float: value of characteristic function
+        """
+        return np.cosh(x) - 1
+
+    def Phi_exp(self, x):
+        """characteristic function for symmetric exponential distribution
+
+        Args:
+            x (float): position at which to evaluate the characteric function
+
+        Returns:
+            float: value of characteristic function
+        """
+        return x**2 / (2 * (1 - x**2))
+
+    def Phi_gamma(self, x):
+        """characteristic function for gamma distribution
+
+        Args:
+            x (float): position at which to evaluate the characteric function
+
+        Returns:
+            float: value of characteristic function
+        """
+        return ((1 + x) ** self.b + (1 - x) ** self.b - 2) / (2 * self.b * (self.b - 1))
+
+    def Phi_truncated(self, x):
+        """characteristic function for truncated distribution
+
+        Args:
+            x (float): position at which to evaluate the characteric function
+
+        Returns:
+            float: value of characteristic function
+        """
+        return 0.5 * x**2 + self.b * x**4
+
+    def F(self, t, s, p):
+        """System function
+
+        Args:
+            t (np.ndarray): time
+            s (np.ndarray): system array
+
+        Returns:
+            np.ndarray: system equations
+        """
+        # q coordinate
+        output1 = (
+            s[1] * self.D1
+            - misc.derivative(self.potential, s[0], dx=self.dx)
+            + self.lamb * self.a * misc.derivative(self.phi, s[1] * self.a, dx=self.dx)
+        )
+
+        # k
+        output2 = s[1] * misc.derivative(self.potential, s[0], self.dx, n=2)
+
+        # output
+        return np.vstack((output1, output2))
+
+    def Residuals(self, ini, fin, p):
+        """Residuals for boundary conditions
+
+        Args:
+            ini (np.ndarray): initial values
+            fin (np.ndarray): final values
+
+        Returns:
+            np.ndarray: residuals
+        """
+        res = np.zeros(3)
+        res[0] = ini[0] - self.boundary_cond[0]
+        res[1] = fin[0] - self.boundary_cond[1]
+        res[2] = ini[1] - p[0]
+        return res
+
+    def instanton(self):
+        """Function to recover the instanton of the system given some initial conditions. The method used is the
+        boundary value problem solver by scipy, see shooting method
+
+        Returns:
+            scipy.integrate._bvp.BVPResult: Result from the integrator
+        """
+        # time
+        t = np.linspace(0, self.maxtime, self.number_timestep)
+
+        # value array
+        y = np.zeros((2, t.size))
+        # apply initial guess
+        y[0, 0] = self.boundary_cond[0]
+        # y[1, 0] = self.boundary_cond[2]
+
+        result = solve_bvp(self.F, self.Residuals, t, y, p=[0.2], verbose=2)
+
+        print(result.message)
+        return result, t
+
+
+mem = NG_memory(lam=1, a=1, maxtime=10, noise="d", number_timestep=30)
+mem_G = NG_memory(lam=0, a=1, maxtime=10, noise="d", number_timestep=30)
+instanton_mem, t = mem.instanton()
+instanton_mem_G, t = mem_G.instanton()
+
+nomem = NG_no_memory(lam=1, a=1, maxtime=10, noise="d", number_timestep=30)
+nomem_G = NG_no_memory(lam=0, a=1, maxtime=10, noise="d", number_timestep=30)
+instanton_no_mem, t = nomem.instanton()
+instanton_no_mem_G, t = nomem_G.instanton()
+
+
+plt.plot(t, instanton_mem.sol(t)[0], label="OU, NG")
+# plt.plot(t, instanton_mem_G.sol(t)[0], "--", label="OU, Gaussian")
+plt.plot(t, instanton_no_mem.sol(t)[0], label="non-OU, NG")
+plt.plot(t, instanton_no_mem_G.sol(t)[0], "--", label="non-OU, Gaussian")
+
+plt.legend()
+plt.grid()
+plt.xlabel(r"$t$")
+plt.ylabel(r"$q$")
+
+plt.show()
